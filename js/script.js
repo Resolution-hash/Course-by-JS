42c010f32d0cda08c40cbffb7536c3aded3bbc5a
leo <example.com>
2021-11-25 18:29:42 +0300

    first task

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 /*jshint globalstrict: true*/
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?',[]);
+console.log(numberOfFilms);
 
-console.log(typeof(4 + 'dfgdfg'));
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let a = prompt('Один из последних просмотренных фильмов?',[]),
+    b = prompt('На сколько оцените его?',[]),
+    c = prompt('Один из последних просмотренных фильмов?',[]),
+    d = prompt('На сколько оцените его?',[]);
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
