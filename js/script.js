6d04ed1026f31b5761a959fbe1c8565e0519d6be
Leonid <example@gmail.com>
2022-01-16 22:56:41 +0300

    task cycle #015 completed!

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 "use strict";
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
-console.log(numberOfFilms);
+let numberOfFilms = 0;
+do{
+    numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?","");
+}while(numberOfFilms == null || numberOfFilms.length == 0);
 
 const personalMovieDB = {
     count:numberOfFilms,
@@ -11,14 +13,37 @@ const personalMovieDB = {
     private:false
 };
 
-console.log(personalMovieDB);
+if(personalMovieDB.count < 10){
+    alert('Просмотренно довольно мало фильмов');
+}else if(personalMovieDB.count > 30){
+    alert('Вы киноман');
+}else if( personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert('Вы классический зритель');
+}
 
-const a = prompt("Один из последних посмотренных фильмов",""),
-      b = prompt("На сколько оцените его?",""),
-      c = prompt("Один из последних посмотренных фильмов",""),
-      d = prompt("На сколько оцените его?","");
+let a,
+    b;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for(let i = 0; i < 2; i++){
+
+    do{
+        a = prompt("Один из последних посмотренных фильмов","");
+        console.log(a);
+        // console.log(a.length);
+        if(a !== null && a.length != 0 && a.length <= 50){
+            do{
+                b = prompt("На сколько оцените его?","");
+            }while(b == null || b.length == 0 || b.length > 50);
+        }
+    }while(a == null || a.length == 0 || a.length > 50);
+
+    personalMovieDB.movies[a] = b;
+}
 
 console.log(personalMovieDB.movies);
+
+
+    
+
+
+
